13fe581f2666062b08a714b8e9d2d385d5924b9d
nac <chotpisit080916c@gmail.com>
2023-07-16 22:32:58 +0700

    feat(client): add layout

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,11 +1,21 @@
-export default function RootLayout({
-  children,
-}: {
+import { Metadata } from "next";
+import { AppProvider } from "../core/context/appProvider";
+
+export const metadata: Metadata = {
+  title: "Nacnano",
+  description: "Nacnano's Personal Website",
+};
+
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <AppProvider>
+        <body>{children}</body>
+      </AppProvider>
     </html>
   );
 }
