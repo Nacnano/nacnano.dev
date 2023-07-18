a52fc7448492b9de81e4f726c607f6a916371d09
nac <chotpisit080916c@gmail.com>
2023-07-19 00:02:49 +0700

    feat(cilent): update tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
