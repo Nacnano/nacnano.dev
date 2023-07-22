d9ed946d8620f71b5d2998f08e89aafe355493e9
nac <chotpisit080916c@gmail.com>
2023-07-22 23:56:54 +0700

    feat(client): add navbar for index page

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from "next";
 import { LandingPage } from "@/modules/LandingPage";
+import { NavigationBar } from "@/components/NavigationBar";
 
 const IndexPage: NextPage = () => {
   return (
     <>
+      <NavigationBar />
       <LandingPage />
     </>
   );
