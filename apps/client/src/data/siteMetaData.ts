05385bfd0a67891b13ead94d7e06a00da683e98f
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-11-15 09:58:42 +0700

    feat: add metadata

---
+++ apps/client/src/data/siteMetaData.ts
@@ -0,0 +1,19 @@
+const siteMetaData = {
+  title: "Nacnano.dev",
+  author: "Nacnano",
+  headerTitle: "Nacnano.dev",
+  description: "Nacnano's personal website",
+  language: "en-us",
+  theme: "system",
+  siteUrl: "https://nacnano-dev-client-nacnano.vercel.app/",
+  siteRepo: "https://github.com/nacnano/nacnano.dev",
+  siteLogo: "/static/images/logo.png",
+  email: "chotpisit.adu@gmail.com",
+  facebook: "https://www.facebook.com/chotpisit.adunsehawat/",
+  instagram: "https://www.instagram.com/chotpisit_nac/",
+  github: "https://www.github.com/nacnano",
+  linkedin: "https://www.linkedin.com/in/chotpisit-adunsehawat-b68912210/",
+  youtube: "https://www.youtube.com/channel/UC35blZ3e07Srxg_bbdXLZKQ",
+};
+
+//TODO: Add Analytics
