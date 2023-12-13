366c0076ccd05495ee7f5f899f3883182ec9c9b1
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-13 17:21:17 +0700

    feat: add robots in layout

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -28,6 +28,19 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
   },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 type RootLayoutProps = {
