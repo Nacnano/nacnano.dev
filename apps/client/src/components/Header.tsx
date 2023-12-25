cf29781491ff00b0b903bb774924b79b2f603876
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-25 20:52:29 +0700

    feat: add custom link

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,18 +1,14 @@
 import { siteMetaData } from "@/data/siteMetaData";
-import Link from "next/link";
-import { Logo } from "./Logo";
 import headerNavLinks from "@/data/headerNavLinks";
+import Logo from "./Logo";
+import Link from "./Link";
 import ThemeSwitch from "./ThemeSwitch";
 
-export default function Header() {
+const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
-        <Link
-          href="/"
-          aria-label={siteMetaData.headerTitle}
-          style={{ textDecoration: "none" }}
-        >
+        <Link href="/" aria-label={siteMetaData.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
               <Logo />
@@ -27,12 +23,11 @@ export default function Header() {
           </div>
         </Link>
       </div>
-      <div className="flex items-center space-x-4 leading-5">
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         {headerNavLinks
           .filter((link) => link.href !== "/")
           .map((link) => (
             <Link
-              style={{ textDecoration: "none" }}
               key={link.title}
               href={link.href}
               className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
@@ -44,4 +39,54 @@ export default function Header() {
       </div>
     </header>
   );
-}
+};
+
+export default Header;
+
+// import { siteMetaData } from "@/data/siteMetaData";
+// import Link from "next/link";
+// import { Logo } from "./Logo";
+// import headerNavLinks from "@/data/headerNavLinks";
+// import ThemeSwitch from "./ThemeSwitch";
+
+// export default function Header() {
+//   return (
+//     <header className="flex items-center justify-between py-10">
+//       <div>
+//         <Link
+//           href="/"
+//           aria-label={siteMetaData.headerTitle}
+//           style={{ textDecoration: "none" }}
+//         >
+//           <div className="flex items-center justify-between">
+//             <div className="mr-3">
+//               <Logo />
+//             </div>
+//             {typeof siteMetaData.headerTitle === "string" ? (
+//               <div className="hidden h-6 text-2xl font-semibold sm:block">
+//                 {siteMetaData.headerTitle}
+//               </div>
+//             ) : (
+//               siteMetaData.headerTitle
+//             )}
+//           </div>
+//         </Link>
+//       </div>
+//       <div className="flex items-center space-x-4 leading-5">
+//         {headerNavLinks
+//           .filter((link) => link.href !== "/")
+//           .map((link) => (
+//             <Link
+//               style={{ textDecoration: "none" }}
+//               key={link.title}
+//               href={link.href}
+//               className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+//             >
+//               {link.title}
+//             </Link>
+//           ))}
+//         <ThemeSwitch />
+//       </div>
+//     </header>
+//   );
+// }
