6ba805aa7d30963f45b245d8e703c43d73d1df5c
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-29 00:31:14 +0700

    style: add tailwind to projects

--- apps/client/src/app/projects/page.tsx
+++ apps/client/src/app/projects/page.tsx
@@ -6,13 +6,19 @@ export const metadata = genPageMetaData({ title: "Projects" });
 export default function Projects() {
   return (
     <>
-      <div>
-        <div>
-          <h1>Projects</h1>
-          <p>Some examples of projects I've done or contributed to</p>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Projects
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Examples of something I've done or contributed to
+          </p>
         </div>
-        <div>
-          <div> {projectsData.map((project) => project.title)}</div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((project) => project.title)}
+          </div>
         </div>
       </div>
     </>
