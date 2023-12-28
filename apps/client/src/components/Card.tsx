25a99ec02bd7fa62721347ce21daf73014338308
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-29 01:10:05 +0700

    feat: style card component

--- apps/client/src/components/Card.tsx
+++ apps/client/src/components/Card.tsx
@@ -4,21 +4,49 @@ import CustomLink from "@/components/Link";
 const Card = ({ title, description, imgSrc, href }) => {
   return (
     <>
-      <div>
-        <div>
+      <div className="md max-w-[544px] p-4 md:w-1/2">
+        <div
+          className={`${
+            imgSrc && "h-full"
+          }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+        >
           {imgSrc &&
             (href ? (
-              <CustomLink href={href} aria-label={`Link to ${title}`}>
+              <CustomLink
+                href={href}
+                aria-label={`Link to ${title}`}
+                className="object-cover object-center md:h-36 lg:h-48"
+              >
                 <Image src={imgSrc} alt={title} width={544} height={306} />
               </CustomLink>
             ) : (
-              <Image src={imgSrc} alt={title} width={544} height={306} />
+              <Image
+                src={imgSrc}
+                alt={title}
+                width={544}
+                height={306}
+                className="object-cover object-center md:h-36 lg:h-48"
+              />
             ))}
-          <div>
-            <h2> {title}</h2>
-            <p>{description}</p>
+          <div className="p-6">
+            <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+              {href ? (
+                <CustomLink href={href} aria-label={`Link to ${title}`}>
+                  {title}
+                </CustomLink>
+              ) : (
+                title
+              )}
+            </h2>
+            <p className="prose dark:prose-invert mb-3 max-w-none text-gray-500 dark:text-gray-400">
+              {description}
+            </p>
             {href && (
-              <CustomLink href={href} aria-label={`Link to ${title}`}>
+              <CustomLink
+                href={href}
+                aria-label={`Link to ${title}`}
+                className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+              >
                 Learn more &rarr;
               </CustomLink>
             )}
--- apps/client/src/data/projectsData.ts
+++ apps/client/src/data/projectsData.ts
@@ -1,13 +1,15 @@
 const projectsData = [
   {
     title: "CU Get Rekt",
-    description: "AJSKHDKJASHDHJASD",
+    description:
+      "Thinc's 10 days project for making student's withdrawals and resignations more efficient and easier by using its automated pdf generator and score prediction features. My role was designing, developing, deploying, and maintaining its backend using Nestjs and Prisma.",
     imgSrc: "/static/images/cugetrekt.jpg",
     href: "https://cugetrekt.vercel.app/",
   },
   {
     title: "CU Get Rekt",
-    description: "AJSKHDKJASHDHJASD",
+    description:
+      "Thinc's 10 days project for making student's withdrawals and resignations more efficient and easier by using its automated pdf generator and score prediction features. My role was designing, developing, deploying, and maintaining its backend using Nestjs and Prisma.",
     imgSrc: "/static/images/cugetrekt.jpg",
     href: "https://cugetrekt.vercel.app/",
   },
