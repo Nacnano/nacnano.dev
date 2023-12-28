b72ebe9429364cf6b265d73d2d650b20d29b3685
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-29 00:54:27 +0700

    fix: change project images path

--- apps/client/src/data/projectsData.ts
+++ apps/client/src/data/projectsData.ts
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: "CU Get Rekt",
     description: "AJSKHDKJASHDHJASD",
-    imgSrc: "/static/img/cugetrekt.jpg",
+    imgSrc: "/static/images/cugetrekt.jpg",
     href: "https://cugetrekt.vercel.app/",
   },
   {
     title: "CU Get Rekt",
     description: "AJSKHDKJASHDHJASD",
-    imgSrc: "/static/img/cugetrekt.jpg",
+    imgSrc: "/static/images/cugetrekt.jpg",
     href: "https://cugetrekt.vercel.app/",
   },
 ];
