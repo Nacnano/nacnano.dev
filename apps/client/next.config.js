b58d96fb5365cf18f53f49f7f2be81e08781f3df
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-28 16:09:34 +0700

    feat: add contentlayer and sample data

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true
+const { withContentlayer } = require('next-contentlayer')
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+module.exports = () => {
+  const plugins = [withContentlayer]
+  return plugins.reduce((acc, next) => next(acc), {
+    reactStrictMode: true,
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    eslint: {
+      dirs: ['src/app', 'src/components', 'src/layouts', 'src/scripts']
+    }
+  })
 }
