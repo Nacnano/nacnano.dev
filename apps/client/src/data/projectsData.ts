48df37ad295b900ae0605b6e0e0366be06a34232
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-29 01:17:21 +0700

    feat: add new datas

--- apps/client/src/data/projectsData.ts
+++ apps/client/src/data/projectsData.ts
@@ -7,11 +7,18 @@ const projectsData = [
     href: "https://cugetrekt.vercel.app/",
   },
   {
-    title: "CU Get Rekt",
+    title: "School Computer Science Project",
     description:
-      "Thinc's 10 days project for making student's withdrawals and resignations more efficient and easier by using its automated pdf generator and score prediction features. My role was designing, developing, deploying, and maintaining its backend using Nestjs and Prisma.",
+      "Usage of MASK R-CNN model for Image Segmentation and Random Forest model for Factor Identification to analyse multimedia effects on students' learning efficiency",
     imgSrc: "/static/images/cugetrekt.jpg",
-    href: "https://cugetrekt.vercel.app/",
+    href: "https://github.com/Nacnano/predicting-and-comparing-learners-interest-in-note-taking-from-multimedia-using-a-machine-learning-",
+  },
+  {
+    title: "MWIT29 Archive",
+    description:
+      "A website for archiving memorable information from my high school by allowing the students to edit their data using NextJS with MongoDB and Google Cloud Storage (Working in Process)",
+    imgSrc: "/static/images/cugetrekt.jpg",
+    href: "https://mwit29-archive.vercel.app/",
   },
 ];
 
