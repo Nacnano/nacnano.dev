19a2e41e24e9ac1e932ed78161132c69104d9c66
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-30 17:00:13 +0700

    style: add css to blog lists

--- apps/client/src/layouts/ListLayout/ListLayout.tsx
+++ apps/client/src/layouts/ListLayout/ListLayout.tsx
@@ -76,28 +76,35 @@ export default function ListLayout({
             const { path, date, title, summary, tags } = blog;
 
             return (
-              <li key={path}>
-                <article>
+              <li key={path} className="py-4">
+                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                   <dl>
-                    <dt> Published on </dt>
-                    <dd>
+                    <dt className="sr-only"> Published on </dt>
+                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                       <time dateTime={date}>
                         {formatDate(date, siteMetadata.locale)}
                       </time>
                     </dd>
                   </dl>
-                  <div>
+                  <div className="space-y-3 xl:col-span-3">
                     <div>
-                      <h3>
-                        <CustomLink href={path}>{title}</CustomLink>
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                        <CustomLink
+                          href={path}
+                          className="text-gray-900 dark:text-gray-100"
+                        >
+                          {title}
+                        </CustomLink>
                       </h3>
-                      <div>
+                      <div className="flex flex-wrap">
                         {tags?.map((tag) => (
                           <Tag key={tag} text={tag} />
                         ))}
                       </div>
                     </div>
-                    <div>{summary}</div>
+                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                      {summary}
+                    </div>
                   </div>
                 </article>
               </li>
