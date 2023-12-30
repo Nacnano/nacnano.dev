32260b9fdfec6452ef44a36a739da29d2b20ec83
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-30 19:00:18 +0700

    feat: add [page]

---
+++ apps/client/src/app/blogs/page/[page]/page.tsx
@@ -0,0 +1,38 @@
+import ListLayout from "@/layouts/ListLayout";
+import { allBlogs } from "contentlayer/generated";
+import { allCoreContent, sortPosts } from "pliny/utils/contentlayer";
+
+const BLOGS_PER_PAGE = 5;
+
+export const generateStaticParams = async () => {
+  const totalPages = Math.ceil(allBlogs.length / BLOGS_PER_PAGE);
+  const paths = Array.from({ length: totalPages }, (_, i) => ({
+    page: (i + 1).toString(),
+  }));
+
+  return paths;
+};
+
+export default function ({ params }: { params: { page: number } }) {
+  const blogs = allCoreContent(sortPosts(allBlogs));
+  const pageNumber = params.page;
+  const initialDisplayBlogs = blogs.slice(
+    BLOGS_PER_PAGE * (pageNumber - 1),
+    BLOGS_PER_PAGE * pageNumber
+  );
+
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(blogs.length / BLOGS_PER_PAGE),
+  };
+  return (
+    <>
+      <ListLayout
+        blogs={blogs}
+        title="All Blogs"
+        pagination={pagination}
+        initialDisplayBlogs={initialDisplayBlogs}
+      />
+    </>
+  );
+}
