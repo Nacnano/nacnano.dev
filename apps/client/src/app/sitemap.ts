10ca17569ec89187cf6e88f9ddab2e6e34207d98
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-31 11:58:19 +0700

    feat: add blogs routes to sitemap

--- apps/client/src/app/sitemap.ts
+++ apps/client/src/app/sitemap.ts
@@ -1,22 +1,19 @@
 import { MetadataRoute } from "next";
 import siteMetadata from "@/data/siteMetadata";
+import { allBlogs } from "contentlayer/generated";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
 
-  // TODO: Add blog posts to sitemap
-  //   const blogRoutes = allBlogs.map((post) => ({
-  //     url: `${siteUrl}/${post.path}`,
-  //     lastModified: post.lastmod || post.date,
-  //   }));
+  const blogRoutes = allBlogs.map((post) => ({
+    url: `${siteUrl}/${post.path}`,
+    lastModified: post.lastmod || post.date,
+  }));
 
   const routes = ["", "blog", "projects", "tags"].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [
-    ...routes,
-    //...blogRoutes
-  ];
+  return [...routes, ...blogRoutes];
 }
