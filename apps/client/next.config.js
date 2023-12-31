34736293c450dc21d616d28389e3f60dd5b76307
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-12-31 18:12:47 +0700

    chore: update next config to module type

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,15 +1,11 @@
-const { withContentlayer } = require('next-contentlayer')
+import { withContentlayer } from 'next-contentlayer'
 
-/**
- * @type {import('next/dist/next-server/server/config').NextConfig}
- **/
-module.exports = () => {
-  const plugins = [withContentlayer]
-  return plugins.reduce((acc, next) => next(acc), {
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['src/app', 'src/components', 'src/layouts', 'src/scripts']
-    }
-  })
+const config = {
+  reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  eslint: {
+    dirs: ['src/app', 'src/components', 'src/layouts', 'src/scripts']
+  }
 }
+
+export default withContentlayer(config)
