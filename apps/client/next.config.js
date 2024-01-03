4ba7596a2ba970f83c013904a604362847f147b2
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-03 23:50:22 +0700

    fix: remove font-src

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -5,7 +5,6 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
   style-src 'self' 'unsafe-inline';
-  font-src 'self' https://fonts.gstatic.com;
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
