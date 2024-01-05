8b0dfe560727ff17d023e32d3fa13324aafc1354
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-05 19:51:11 +0700

    feat: update robot file

--- apps/client/src/app/robots.ts
+++ apps/client/src/app/robots.ts
@@ -1,13 +1,13 @@
-import siteMetadata from "@/data/siteMetadata";
 import { MetadataRoute } from "next";
+import siteMetadata from "@/data/siteMetadata";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
     },
-    sitemap: siteMetadata.siteUrl + "sitemap.xml",
+    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+    host: siteMetadata.siteUrl,
   };
 }
