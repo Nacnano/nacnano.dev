f778d8026f9aba2699d5fb572c9499fb612cf2f5
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-06 14:50:56 +0700

    docs: add comment to createTagJson file

--- apps/client/src/scripts/createTagJson.mjs
+++ apps/client/src/scripts/createTagJson.mjs
@@ -1,3 +1,8 @@
+// A temporary script to create a JSON file with the tag count for each tag
+// Use for development in window devices
+// since the linux deployment ran the script automatically with contentlayer build.
+// For running in window, cd in to apps/client and run "node ./src/scripts/createTagJson.mjs"
+
 import { allBlogs } from '../../.contentlayer/generated/index.mjs'
 import { writeFileSync } from 'fs'
 import GithubSlugger from 'github-slugger'
