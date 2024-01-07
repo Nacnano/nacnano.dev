9b08cf69939a34483c24c2f23f03c145d2079100
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-07 21:14:49 +0700

    style: minor items center

--- apps/client/src/layouts/ListLayout/ListLayout.tsx
+++ apps/client/src/layouts/ListLayout/ListLayout.tsx
@@ -127,7 +127,7 @@ export default function ListLayout({
             return (
               <li key={path} className="py-4">
                 <article className=" space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <dl className="flex flex-wrap">
+                  <dl className="flex flex-wrap items-center">
                     <dt className="sr-only"> Published on </dt>
                     <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                       <time className="text-lg mr-3" dateTime={date}>
