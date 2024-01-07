175df026ecd133b5e710f19a4c4fc7126da84664
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-07 16:00:19 +0700

    fix: import new letter form in mdx components

--- apps/client/src/components/MDXComponents.tsx
+++ apps/client/src/components/MDXComponents.tsx
@@ -2,7 +2,7 @@ import type { MDXComponents } from "mdx/types";
 import Image from "./Image";
 import TOCInline from "pliny/ui/TOCInline";
 import Pre from "pliny/ui/Pre";
-import BlogNewsLetterForm from "pliny/ui/BlogNewsLetterForm";
+import BlogNewsletterForm from "pliny/ui/BlogNewsletterForm";
 import CustomLink from "./Link";
 import TableWrapper from "./TableWrapper";
 
@@ -12,5 +12,5 @@ export const components: MDXComponents = {
   a: CustomLink,
   pre: Pre,
   table: TableWrapper,
-  BlogNewsLetterForm,
+  BlogNewsletterForm,
 };
