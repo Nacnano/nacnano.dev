c15424befd5f7024f54a36be3fe673cd03d43a01
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-11 10:37:15 +0700

    feat: add images for projects

--- apps/client/src/data/projectsData.ts
+++ apps/client/src/data/projectsData.ts
@@ -3,21 +3,21 @@ const projectsData = [
     title: "CU Get Rekt",
     description:
       "Thinc's 10 days project for making student's withdrawals and resignations more efficient and easier by using its automated pdf generator and score prediction features. My role was designing, developing, deploying, and maintaining its backend using Nestjs and Prisma.",
-    imgSrc: "/static/images/cugetrekt.jpg",
+    imgSrc: "/static/images/cugetrekt.png",
     href: "https://cugetrekt.vercel.app/",
   },
   {
-    title: "School Computer Science Project",
+    title: "Computer Science Project",
     description:
       "Usage of MASK R-CNN model for Image Segmentation and Random Forest model for Factor Identification to analyse multimedia effects on students' learning efficiency",
-    imgSrc: "/static/images/cugetrekt.jpg",
+    imgSrc: "/static/images/computer-project.png",
     href: "https://github.com/Nacnano/predicting-and-comparing-learners-interest-in-note-taking-from-multimedia-using-a-machine-learning-",
   },
   {
-    title: "MWIT29 Archive",
+    title: "MWIT29 Archive (WIP)",
     description:
       "A website for archiving memorable information from my high school by allowing the students to edit their data using NextJS with MongoDB and Google Cloud Storage (Working in Process)",
-    imgSrc: "/static/images/cugetrekt.jpg",
+    imgSrc: "/static/images/mwit29-archive.png",
     href: "https://mwit29-archive.vercel.app/",
   },
 ];
