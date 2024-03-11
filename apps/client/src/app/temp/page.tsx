a8579b621c09ec5023fe8ddad030ccfa7cef2a37
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-03-11 12:12:55 +0700

    feat: remove use client from temp link

--- apps/client/src/app/temp/page.tsx
+++ apps/client/src/app/temp/page.tsx
@@ -1,8 +1,6 @@
-"use server";
-
 import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default function Page() {
   const link =
     "https://drive.google.com/drive/folders/1Beobo5-Qe79ec_w4Zz8U9CSqISAHsAJF?usp=sharing";
   redirect(link);
