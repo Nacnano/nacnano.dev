efcb7b2397cde681e6b8fc4a9d8789983e545666
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-03-11 12:15:21 +0700

    feat: add link page

--- apps/client/src/app/temp/page.tsx
+++ apps/client/src/app/temp/page.tsx
@@ -4,4 +4,6 @@ export default function Page() {
   const link =
     "https://drive.google.com/drive/folders/1Beobo5-Qe79ec_w4Zz8U9CSqISAHsAJF?usp=sharing";
   redirect(link);
+
+  return <>Redirecting</>;
 }
