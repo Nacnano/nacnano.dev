86e874b65d03fa7e6c454eeb529dfb95539ae183
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-03-11 15:38:00 +0700

    fix: minor ui problem

--- apps/client/src/app/link/page.tsx
+++ apps/client/src/app/link/page.tsx
@@ -5,14 +5,14 @@ export default function Page() {
   return (
     <>
       <h1 className="flex flex-col justify-center items-center font-bold text-xl pb-5">
-        Keys Page
+        All Links
       </h1>
       <div className="flex flex-row justify-center items-center flex-wrap gap-5 ">
         {links &&
           Object.entries(links).map(([key, link]) => (
             <CustomLink
               key={key}
-              className="text-blue-300 hover:text-blue-600 hover:text-lg"
+              className="text-blue-300 hover:text-blue-600 text-lg"
               href={link}
             >
               {key}
