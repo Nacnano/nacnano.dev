039cc4077c01471d000e92a384923d40fba4f66b
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-03-11 15:36:15 +0700

    feat: add static params for link

--- apps/client/src/app/link/[link]/page.tsx
+++ apps/client/src/app/link/[link]/page.tsx
@@ -1,6 +1,11 @@
 import { redirect } from "next/navigation";
 import links from "../links.json";
 
+export async function getStaticParams() {
+  const paths = Object.keys(links).map((link) => ({ link }));
+  return paths;
+}
+
 export default function Page({ params }: { params: { link: string } }) {
   redirect(links[params.link]);
 }
