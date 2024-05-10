2ebcf5474e7b56ea83106c2dc7430695501bbc22
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-05-10 18:05:58 +0700

    fix: remove static param from link page

--- apps/client/src/app/link/[link]/page.tsx
+++ apps/client/src/app/link/[link]/page.tsx
@@ -1,11 +1,6 @@
 import { redirect } from "next/navigation";
 import links from "../links.json";
 
-export async function getStaticParams() {
-  const paths = Object.keys(links).map((link) => ({ link }));
-  return paths;
-}
-
 export default function Page({ params }: { params: { link: string } }) {
   redirect(links[params.link]);
 }
