7c95fd677d2f37f85e74f36c8d22ea473e9a833a
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-06-25 00:47:07 +0700

    docs: add timelineData

--- apps/client/src/data/timelineData.ts
+++ apps/client/src/data/timelineData.ts
@@ -3,12 +3,55 @@ export const timelineItems = [
     date: "June, 2024 - Present",
     title: "Software Engineer Intern",
     location: "Agoda",
-    description: "Intern goes brrrrr",
+    description:
+      "Migrating backend services to Kotlin. Developing internal tool using React for account data management.",
+  },
+  {
+    date: "June, 2023 - August, 2023",
+    title: "Full Stack Developer Intern",
+    location: "Wang Data Market",
+    description:
+      "Implemented form features with React and API endpoints with ExpressJS",
+  },
+  {
+    date: "March, 2022 - August, 2022",
+    title: "Full Stack Developer and Data Scientist Intern",
+    location: "MonkeyEveryday",
+    description:
+      "Developed TCAS Ranking algorithm. Conducted data analysis with PostgreSQL. Automated user data updates using Cypress. Maintained websites with VueJS and Firebase.",
   },
   {
     date: "2022 - Present",
-    title: "Computer Engineering Student",
+    title: "Computer Engineering",
     location: "Chulalongkorn University",
-    description: "4.00 GPAX (3 semesters) ayyy",
+    description:
+      "4.00 GPA for 3 semesters, TA for software development courses.",
+  },
+  {
+    date: "2019 - 2022",
+    title: "High School",
+    location: "Mahidol Wittayanusorn School",
+    description:
+      "4.00 GPA, participated in exchange programs and competitions.",
+  },
+  {
+    date: "2019 - 2022",
+    title: "Computer Olympiad",
+    location: "Kasetsart University and Mahidol Wittayanusorn School",
+    description:
+      "Qualified for POSN Computer Olympiad second camp. Placed first reserved for the 17th Thailand Olympiad in Informatics",
+  },
+  {
+    date: "2016 - 2019",
+    title: "Middle School",
+    location: "Suankularb Wittayalai School",
+    description: "3.56 GPA.",
+  },
+
+  {
+    date: "2013 - 2016",
+    title: "Primary School",
+    location: "Anuban Samut Sakhon School",
+    description: "3.97 GPA.",
   },
 ];
